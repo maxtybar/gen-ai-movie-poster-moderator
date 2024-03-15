1105cae39d1efc972c10ff8e23a50f33447e5632
Max Tybar <maxtybar@gmail.com>
2024-03-14 17:25:37 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
-# gen-ai-movie-poster-moderator
+# Gen AI Movie Poster Moderator
 Flag poster that are not in semantic compliance with custom policies using Gen AI.
+
+# Prerequisites
+
+Deployment has been tested on MacOS, Windowsa and Linux machines. Installation guide assumes you have AWS account and Administrator Access to provision all the resources. 
+
+=============
+
+* [Amazon Bedrock Claude V3 Sonnet](https://www.aboutamazon.com/news/aws/amazon-bedrock-anthropic-ai-claude-3) - access to the Anthropic's Claude 3 Sonnet model on Amazon Bedrock
+* [node](https://docs.npmjs.com/downloading-and-installing-node-js-and-npm) >= 16.0.0
+* [Python](https://www.python.org/) >= 3.11
+* [pip](https://pypi.org/project/pip/) >= 23.3.1
+* [AWS CLI](https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html) >= 2.0.0
+* [AWS CDK](https://docs.aws.amazon.com/cdk/v2/guide/getting_started.html) >= 2.66.1
+
+# Before you start
+
+Clone current repository:
+
+```
+git clone https://github.com/maxtybar/gen-ai-movie-poster-moderator.git
+```
+
+Navigate to the cloned repository in your terminal/shell. All of the commands are to be executed from the project's root folder/repo that you just cloned.
+
+Recommended: Create a Python virtual environment. To manually create a virtualenv run the following command:
+
+```
+python3 -m venv .venv
+```
+
+After the init process completes and the virtualenv is created, you can use the following
+step to activate your virtualenv.
+
+**Note: This command is for MacOS/Linux machines only.** 
+If you are using MacOS/Linux machine run the following command:
+
+```
+source .venv/bin/activate
+```
+
+**Note: This command is for Windows machines only.** 
+If you are using a Windows platform, you would activate the virtualenv like this:
+
+```
+.venv\Scripts\activate.bat
+```
+
+Once the virtualenv is activated, you can install the required dependencies.
+
+```
+python3 -m pip install -r requirements.txt
+```
+
+To run the application execute the following command:
+
+```
+python3 app.py
+```
+
+Navigate to the local host URL at http://127.0.0.1:7860.
+
+
---
+++ app.py
@@ -0,0 +1,103 @@
+import os
+import gradio as gr
+import boto3
+import json
+import base64
+import io
+import pandas as pd
+
+bedrock = boto3.client(
+    service_name='bedrock-runtime',
+    region_name='us-west-2'
+)
+
+
+def image_mod(image):
+    byte_arr = io.BytesIO()
+    image.save(byte_arr, format='JPEG')
+    byte_arr = byte_arr.getvalue()
+    image_b64_string = base64.b64encode(byte_arr).decode("utf-8")
+
+    modelId = 'anthropic.claude-3-sonnet-20240229-v1:0'
+    contentType = 'application/json'
+    accept = 'application/json'
+
+    prompt = '''
+            Instructions:
+            I'm a compliance bot, I evaluate images and provide results back as a pass or fail. 
+            Do not offer any explination  Only respond in JSON with the following schema: 
+            
+            [
+            Condition:
+            Status:
+            Reason:
+            Confidence:
+            ]
+
+            For each one of the conditions below return the result as an individual object, each condition is seperated by a comma, 
+            If the below condition is in the picture, return a failed result with the string of fail, 
+            if the below condition is not in the image, return the value pass.  
+            If the below condition is found, provide the reason in the Reason field.  
+            If the below condition is found, provide a Confidence rating between 1 and 100, 
+            100 being the most Confident and 1 being the least confident 
+            
+            Condition: skeletons, person with beard, cigarettes, gun pointed at viewer, sharks, sharks eating humans,
+            adult themes and mature content, bones, gun pointing at user, alcohol
+            '''
+
+    messages = [
+        {
+            "role": "user",
+            "content": [
+                {
+                    "type": "image",
+                    "source": {
+                        "type": "base64",
+                        "media_type": "image/png",
+                        "data": image_b64_string
+                    }
+                },
+                {
+                    "type": "text",
+                    "text": prompt
+                }
+            ]
+        }
+    ]
+
+    body = json.dumps({
+        "anthropic_version": "bedrock-2023-05-31",
+        "max_tokens": 50000,
+        "messages": messages
+    })
+
+    response = bedrock.invoke_model(
+        modelId=modelId,
+        contentType=contentType,
+        accept=accept,
+        body=body
+    )
+
+    # Print response
+    response_body = json.loads(response.get('body').read())
+    response_text = response_body['content'][0]['text']
+    data = json.loads(response_text)
+    df = pd.DataFrame(data)
+    html_table = df.to_html(index=False)
+
+    return html_table
+
+
+image_files = [os.path.join("images", f) for f in os.listdir(
+    "images") if os.path.isfile(os.path.join("images", f))]
+
+demo = gr.Interface(
+    fn=image_mod,
+    inputs=gr.Image(type="pil"),
+    examples=image_files,
+    outputs="html",
+    title="Gen AI Movie Poster Moderator",
+    description="Flag poster that are not in semantic compliance with custom policies using Gen AI.",
+)
+
+demo.launch()
